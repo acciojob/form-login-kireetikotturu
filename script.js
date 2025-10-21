9d793109eb25a6a8330dc58f022563e7c4ec4def
Chandra Kireeti Kotturu <104622340+kireetikotturu@users.noreply.github.com>
2025-10-21 16:35:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
+	const form = document.forms['form1'];
+	let firstName = form.fname.value;
+	let secondName = form.lname.value;
 
+	let fullName = `${firstName} ${secondName}`;
+	alert(fullName);
 }
